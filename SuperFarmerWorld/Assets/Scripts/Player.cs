0d43b8159be378e3d2953574c4c94c0909ef6864
Cleber Tomazoni <clebertomazoni@hotmail.com>
2019-05-20 22:26:23 -0300

    Criação do personagem e plataforma

---
+++ SuperFarmerWorld/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimentar();
+    }
+
+    void Movimentar()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+}
